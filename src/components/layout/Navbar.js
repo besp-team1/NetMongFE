53c0eb392f9df4e1a61d0d93ad6636fc476fe4b7
kkambbak <yunsung9841@gmail.com>
2023-11-28 15:35:20 +0900

    Feat: 로그아웃

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,38 +1,68 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../../style/layout/Navbar.css';
-import logoImage from '../../assets/images/logo.png'; 
+import logoImage from '../../assets/images/logo.png';
+import logoutAPI from "../../API/logoutAPI";
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
 
   const location = useLocation();
   const isMainPage = location.pathname === '/';
+  const isLoggedIn = checkLoggedInStatus();
+  const navigate = useNavigate();
 
-    return (
-        <nav className={`navbar ${isMainPage ? 'main-page' : ''}`}>
-          <Link to="/" className="logo">
-              <img src={logoImage} alt="로고" />
-          </Link>
-        <ul className="menu">
-          <li className="nav-item">
-            <Link to="/post" className="nav-link">포스트</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/market" className="nav-link">마켓</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/api/v1/parks" className="nav-link">내근처</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/mypage" className="nav-link">마이페이지</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/login" className="nav-link">로그인/회원가입</Link>
-          </li>
-        </ul>
-      </nav>
-    );
-    
+  function checkLoggedInStatus() {
+    const token = localStorage.getItem('token');
+
+    return token ? true : false;
   }
+
+  const handleLogout = async(e) => {
+    e.preventDefault();
+    await logoutAPI(localStorage.getItem('token')).then((res)=>{
+        localStorage.removeItem('token');
+        navigate('/');
+      })
+    };
+
+  return (
+    <nav className={`navbar ${isMainPage ? 'main-page' : ''}`}>
+      <Link to="/" className="logo">
+        <img src={logoImage} alt="로고" />
+      </Link>
+      <ul className="menu">
+        <li className="nav-item">
+          <Link to="/post" className="nav-link">포스트</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/market" className="nav-link">마켓</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/api/v1/parks" className="nav-link">내근처</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/mypage" className="nav-link">마이페이지</Link>
+        </li>
+        {/* 로그인 상태에 따라 다른 메뉴를 보여줍니다. */}
+        {isLoggedIn ? (
+          <>
+            <li className="nav-item">
+              님 환영합니다!
+            </li>
+            <li className="nav-item" onClick={handleLogout}>
+              <Link className="nav-link">로그아웃</Link>
+            </li>
+          </>
+        ) : (
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">로그인</Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+
+}
 
 export default Navbar;
