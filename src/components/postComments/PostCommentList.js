919dca09f6543dc3a9757f483b0d374ab7561bbd
jhnyuk <debussysuh@gmail.com>
2023-11-29 10:18:09 +0900

    Chore: 댓글 리스트 가져올 때 토큰 적용

--- src/components/postComments/PostCommentList.js
+++ src/components/postComments/PostCommentList.js
@@ -4,13 +4,14 @@ import '../../style/postComments/PostCommentList.css'
 
 const PostCommentList = ({ postId }) => {
     const [comments, setComments] = useState([]);
+    const authToken = localStorage.getItem('token');
 
     useEffect(() => {
         const fetchComments = async () => {
         try {
             const response = await axios.get(`http://localhost:9000/api/v1/post/comment/${postId}`, {
                 headers: {
-                    Authorization: `Bearer ${`eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1MXMzZTRycnIiLCJhdXRoIjoibWVtYmVyIiwiZXhwIjoxNzA5ODQ4NTMwfQ.h9Xq_74YAnxfqS3MFDZQTT8-CmUiQp12_gGLxZU8tO0TynB4hzGWY__SaasGt7pVmLyu6dFWb4aQXNprPSkrYQ`}`,
+                    Authorization: `Bearer ${authToken}`,
                 },
             });
             setComments(response.data.data);
