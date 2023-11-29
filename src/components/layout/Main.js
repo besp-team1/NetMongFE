e0f3eb8a5b3765f19882d32067ac53d9d9c6bcf7
jhnyuk <debussysuh@gmail.com>
2023-11-29 14:33:16 +0900

    Feat: 눈 내리기 효과

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -3,9 +3,25 @@ import '../../style/layout/Main.css';
 import bannerImage from '../../assets/images/banner.jpg'; 
 
 function Main() {
+  const getRandomNumber = (min, max) => Math.random() * (max - min) + min;
+
+  const snowflakes = Array.from({ length: 100 }).map((_, index) => (
+    <div
+      key={index}
+      className="snowflake"
+      style={{
+        width: `${getRandomNumber(5, 15)}px`, // 눈송이 크기 랜덤 설정
+        height: `${getRandomNumber(5, 15)}px`, // 눈송이 크기 랜덤 설정
+        animationDuration: `${getRandomNumber(5, 15)}s`, // 눈송이 속도 랜덤 설정
+        left: `${getRandomNumber(0, 100)}%`, // 눈송이 가로 위치 랜덤 설정
+      }}
+    ></div>
+  ));
+
   return (
     <div className="main-container">
-        <img src={bannerImage} />
+      <img src={bannerImage} alt="Banner" />
+      {snowflakes}
     </div>
   );
 }
