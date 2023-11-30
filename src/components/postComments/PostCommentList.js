e8be4f2b744d984a53e8a2c3c5baf17ced572203
jhnyuk <debussysuh@gmail.com>
2023-11-30 14:01:50 +0900

    Feat: 댓글 작성시 페이지 로드

--- src/components/postComments/PostCommentList.js
+++ src/components/postComments/PostCommentList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import '../../style/postComments/PostCommentList.css'
+import '../../style/postComments/PostCommentList.css';
 
 const PostCommentList = ({ postId }) => {
     const [comments, setComments] = useState([]);
