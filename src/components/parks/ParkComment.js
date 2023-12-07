d93a1fc0a4d9f3afdbd05a84edc3bafb49ed12b3
chaewon-io <0101angela@naver.com>
2023-12-07 14:47:23 +0900

    Feat: 공원 상세 정보 화면에 표시

--- src/components/parks/ParkComment.js
+++ src/components/parks/ParkComment.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'; 
-import { fetchComments } from '../../API/parkApi';
+import { fetchComments, getPark } from '../../API/parkApi';
 import ParkCommentForm from './ParkCommentForm';
 import ParkCommentList from './ParkCommentList';
 import '../../style/parks/ParkComment.css';
 
 const ParkComment = () => {
     const { parkId } = useParams();
+    const [park, setPark] = useState(null);
     const [comments, setComments] = useState([]);
     const [page, setPage] = useState(1);
     const [flag, setFlag] = useState(false);
@@ -17,6 +18,7 @@ const ParkComment = () => {
 
     useEffect(() => {
          updateComments();
+         updatePark();
     }, [page, flag]);
 
     const register=()=>{
@@ -32,10 +34,19 @@ const ParkComment = () => {
         });
     };
 
+    const updatePark = async () => {
+        const result = await getPark(parkId);
+        console.log(result);
+        setPark(result.data);
+    };
+    
     return (
         <div className="parkComment-container">
             <h2>공원 추천하멍</h2>
             <h6>산책하기 좋은 공원으로 추천합니다!</h6>
+            <p>공원명 : {park ? park.parkNm : 'Loading...'}</p> 
+            <p>주소 : {park ? park.lnmadr : 'Loading...'}</p> 
+            <p>전화번호 : {park ? park.phoneNumber : 'Loading...'}</p> 
             <div className="Parkcomment-container">
                 <ParkCommentList parkId={parkId} comments={comments} updateComments={updateComments} pageInfo={pageInfo} setPage={setPage} page={page} />
                 <ParkCommentForm parkId={parkId} register={register} updateComments={updateComments} />
