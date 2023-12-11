1853600ab83b552f0d365efe51a7ab17108824af
sohhhyeeun <syeankm12076@gmail.com>
2023-12-11 12:53:25 +0900

    Feat: 포스트 상세에 해시태그 검색이 가능하도록 수정

--- src/components/posts/PostDetail.js
+++ src/components/posts/PostDetail.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import '../../style/posts/PostDetail.css';
 import PostCommentForm from '../postComments/PostCommentForm';
 import PostCommentList from '../postComments/PostCommentList';
@@ -141,13 +141,34 @@ const PostDetail = () => {
         return <div>게시글을 불러오는 중...</div>;
     }
 
+    const content = post.content;
+const regex = /#[^\s#]+/g;
+const hashtags = content.match(regex);
+const splitContent = content.split(regex);
+
     return (
         <div className="post-container">
             <h1>{post.title}</h1>
             <img src={`${post.imageUrl}`} alt="게시물 이미지" style={{ maxWidth: '100%', height: 'auto' }} />
             <div className="post-sub">
                 <p>{post.writer}</p>
-                <p>{post.content}</p>
+                <p>
+                    {splitContent.map((part, index) => {
+                        return (
+                            <React.Fragment key={index}>
+                                {part}
+                                {hashtags[index] && (
+                                    <a
+                                        href={`/posts?tag=${encodeURIComponent(hashtags[index])}`}
+                                        className="hashtag-link"
+                                    >
+                                        {hashtags[index]}
+                                    </a>
+                                )}
+                            </React.Fragment>
+                        );
+                    })}
+                </p>
             </div>
             <div className="post-date">
                 <p>{post.createDate}</p>
