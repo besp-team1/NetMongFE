6d2946e43ac3d68b2997b1a3ff5f4d2f1f742598
shinjaewon99 <tlswodnjs99@naver.com>
2023-12-14 15:47:50 +0900

    Feat : 프론트 작업

--- src/components/products/ProductEdit.js
+++ src/components/products/ProductEdit.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 
@@ -14,57 +14,33 @@ function ProductEdit() {
       const { productId } = useParams();
       const navigate = useNavigate();
     
-      // 상품 데이터 불러오기
-      useEffect(() => {
-        const fetchProduct = async () => {
-            try {
-                const config = {
-                  headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                  }
-                };
-            
-                const formData = new URLSearchParams();
-                for (let key in formData) {
-                  formData.append(key, formData[key]);
-                }
-            
-                await axios.patch(`http://localhost:9000/api/v1/products/${productId}`, formData, config);
-                navigate(`api/v1/products`);
-              } catch (error) {
-                console.error('상품 수정 중 오류 발생:', error.message);
-              }
-        };
-        fetchProduct();
-    }, [productId]);
     
     const handleInputChange = (event) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.name === 'price' || event.target.name === 'count' 
-                ? Number(event.target.value)
-                : event.target.name === 'category'
-                ? { name: event.target.value } // 'category' 필드를 객체로 처리
-                : event.target.value
-        });
+      const { name, value } = event.target;
+      setFormData(prevFormData => ({
+        ...prevFormData,
+        [name]: name === 'price' || name === 'count' 
+          ? Number(value)
+          : value
+      }));
     };
     
-     const handleEdit = async () => {
-  try {
-    const formData = new FormData();
-    formData.append('productName', formData.productName);
-    formData.append('price', formData.price);
-    formData.append('content', formData.content);
-    formData.append('count', formData.count);
-    formData.append('category', formData.category);
-
-    await axios.patch(`http://localhost:9000/api/v1/products/${productId}`, formData);
-    navigate(`api/v1/products`);
-  } catch (error) {
-    console.error('상품 수정 중 오류 발생:', error.message);
-  }
-};
-
+    const handleEdit = async () => {
+      try {
+        const updatedFormData = new FormData();
+        updatedFormData.append('productName', formData.productName);
+        updatedFormData.append('price', formData.price);
+        updatedFormData.append('content', formData.content);
+        updatedFormData.append('count', formData.count);
+        updatedFormData.append('category', formData.category);
+    
+        await axios.patch(`http://localhost:9000/api/v1/products/${productId}`, updatedFormData);
+        navigate('/api/v1/products'); // 절대 경로를 사용
+      } catch (error) {
+        console.error('상품 수정 중 오류 발생:', error.message);
+      }
+    };
+    
   return (
     <div>
       <h2>상품 수정</h2>
