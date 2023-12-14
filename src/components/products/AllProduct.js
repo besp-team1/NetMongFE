528bd73e2ba59b553004ac162beb78f7f91f95cd
chaewon-io <0101angela@naver.com>
2023-12-14 15:46:12 +0900

    Feat: 상품 카테고리 조회

--- src/components/products/AllProduct.js
+++ src/components/products/AllProduct.js
@@ -5,9 +5,11 @@ import '../../style/products/ProductList.css';
 
 function ProductList() {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
   const [pageNumber, setPageNumber] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [searchTerm, setSearchTerm] = useState(''); // 상품 검색
+  const [selectedCategory, setSelectedCategory] = useState(''); // 카테고리 검색
 
   const navigate = useNavigate();
 
@@ -44,7 +46,7 @@ function ProductList() {
     setPageNumber(newPageNumber);
   };
 
-  // 이름 검색 추가
+  // 이름 검색
   const fetchProducts = async (url, isSearch = false) => {
     try {
       const response = await axios.get(url);
@@ -69,14 +71,26 @@ function ProductList() {
     fetchProducts(`http://localhost:9000/api/v1/products/name/${searchTerm}`, true);
   };
 
+  // 카테고리 변경
+  const handleInputChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
   return (
     <div className="productList-container">
       <h2>지갑 두둑히 준비 됐멍?</h2>
       <h6>반려견을 위한 마켓에서 안심하고 구매하세요!</h6>
-
-      <button onClick={handleProductRegistration}>상품 등록</button>
-
+      <button className="ProductRegistBtn" onClick={handleProductRegistration}>상품 등록</button>
       <div className="ProductSearch-container">
+        <label className="ProductCategory">
+          <select name="category" value={selectedCategory} onChange={handleInputChange}>
+            <option value="">카테고리 선택</option>
+            <option value="PET_SUPPLIES">반려동물 용품</option>
+            <option value="PET_FEED">반려동물 사료</option>
+            <option value="PET_CLOTHES">반려동물 의류</option>
+            <option value="ETC">기타</option>
+          </select>
+        </label>
         <input
           type="text"
           placeholder="상품명을 입력하세요"
@@ -85,30 +99,33 @@ function ProductList() {
         />
         <button className="ProductNameBtn" onClick={handleSearch}>검색</button>
       </div>
-
       <ul className="products-container">
-        {products.map((product, index) => (
-          <li key={index}>
-            <h3>{product.productName}</h3>
-            <img src={product.imageUrl} alt={product.productName} width="100" height="100" />
-            <p>가격: {product.price}</p>
-            <p>내용: {product.content}</p>
-            <p>상품 갯수: {product.count}</p>
-            <p>카테고리: {product.category}</p>
-            <button onClick={() => handleProductDetails(product.productId)}>상세 보기</button>
-          </li>
-        ))}
+        {products
+          .filter(product => selectedCategory ? product.category === selectedCategory : true)
+          .map((product, index) => (
+            <li key={index}>
+              <h3>{product.productName}</h3>
+              <img src={product.imageUrl} alt={product.productName} width="100" height="100" />
+              <p>가격: {product.price}</p>
+              <p>내용: {product.content}</p>
+              <p>상품 갯수: {product.count}</p>
+              <p>카테고리: {product.category}</p>
+              <button className="productDetailBtn" onClick={() => handleProductDetails(product.productId)}>상세 보기</button>
+            </li>
+          ))}
       </ul>
-
-      {/* Basic pagination controls */}
-      <div>
-        <button onClick={() => handlePageChange(pageNumber - 1)} disabled={pageNumber === 1}>
-          이전 페이지
-        </button>
-        <span> Page {pageNumber} of {totalPages} </span>
-        <button onClick={() => handlePageChange(pageNumber + 1)} disabled={pageNumber === totalPages}>
-          다음 페이지
-        </button>
+      <div className="ProductPagination-container">
+        <button onClick={() => setPage((prevPage) => Math.max(prevPage - 1, 1))}>{"<"}</button>
+        {Array.from({length: totalPages}, (_, i) => i + 1).map((number) => (
+          <button 
+            key={number} 
+            onClick={() => handlePageChange(number)}
+            className={pageNumber === number ? 'active' : ''}
+          >
+            {number}
+          </button>
+        ))}
+        <button onClick={() => setPage((prevPage) => Math.min(prevPage + 1, totalPages))}>{">"}</button>
       </div>
     </div>
   );
